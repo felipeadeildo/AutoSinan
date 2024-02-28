d10f9594479245344600212a2943d1fd554e98b6
sr-pato <felipe.adeildo@proton.me>
2024-02-28 17:54:56 -0300

    Add some mapper constants

--- core/constants.py
+++ core/constants.py
@@ -1,5 +1,5 @@
 import datetime as dt
-from typing import Literal
+from typing import Literal, Mapping
 
 SINAN_BASE_URL = "http://sinan.saude.gov.br"  # protocol http
 """Base url for Sinan Website."""
@@ -26,11 +26,59 @@ CREDENTIALS_FILE = "credentials.json"
 DATA_FOLDER = "dados"
 """Folder name to store the datasets used by the bots."""
 
+POSSIBLE_EXAM_TYPES = Literal["IgM", "NS1", "PCR"]
 
-EXAMS_GAL_MAP: dict[str, Literal["IgM", "NS1", "PCR"]] = {
+EXAMS_GAL_MAP: dict[str, POSSIBLE_EXAM_TYPES] = {
     "Dengue, IgM": "IgM",
     "Dengue, Detecção de Antígeno NS1": "NS1",
     "Dengue, Biologia Molecular": "PCR",
     "Pesquisa de Arbovírus (ZDC)": "PCR",
 }
 """Exam mapping for the Gal dataset."""
+
+EXAM_VALUE_COL_MAP: dict[POSSIBLE_EXAM_TYPES, str] = {
+    "IgM": "Resultado",
+    "NS1": "Resultado",
+    "PCR": "Dengue",
+}
+"""Represents the column name of the exam value in the unificated dataset."""
+
+# 5 = Descartado; 10 = Dengue
+CLASSSIFICATION_MAP: Mapping[POSSIBLE_EXAM_TYPES, Mapping[str, str | None]] = {
+    "IgM": {
+        "Não Reagente": "5",
+        "Reagente": "10",
+        "Indeterminado": None,
+    },
+    "PCR": {
+        "Não Detectável": "5",
+        "Detectável": "10",
+        "_default": None,
+    },
+    "NS1": {
+        "Não Reagente": "5",
+        "Reagente": "10",
+        "Indeterminado": None,
+    },
+}
+"""Classification map to the GAL Exam Result."""
+
+
+EXAM_RESULT_ID: Mapping[POSSIBLE_EXAM_TYPES, Mapping[str, str]] = {
+    "PCR": {
+        "Não Detectável": "2",  # Negativo
+        "Detectável": "1",  # Positivo
+        "_default": "3",  # Inconclusivo
+    },
+    "IgM": {
+        "Não Reagente": "2",  # Não Reagente
+        "Reagente": "1",  # Reagente
+        "Indeterminado": "3",  # Inconclusivo
+    },
+    "NS1": {
+        "Reagente": "1",  # Positivo
+        "Não Reagente": "2",  # Negativo
+        "Indeterminado": "3",  # Inconclusivo
+    },
+}
+"""Input value map for the exam result in unificated dataset to Sinan Investigation Form"""
