5310fff6421f921d8ead9ab2456897c03f8b753e
Felipe Adeildo <felipeadeildo@duck.com>
2024-08-22 19:59:05 -0300

    feat(bots): add router to get bot by slug

--- backend/routers/bots.py
+++ backend/routers/bots.py
@@ -1,6 +1,6 @@
 from typing import Annotated, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from prisma.models import Bot, User
 from prisma.partials import BotSafe
 from services.auth import get_current_user, permission_required
@@ -15,8 +15,22 @@ router = APIRouter()
     "",
     response_model=List[BotSafe],
     summary="Get all the bots",
-    description="Get all the bots informations as name and needed informations",
+    description="Get all the bots informations as name",
 )
 @permission_required("bots:read")
 async def list_bots(current_user: Annotated[User, Depends(get_current_user)]):
     return await Bot.prisma().find_many()
+
+
+@router.get(
+    "/{slug}",
+    response_model=BotSafe,
+    summary="Get a bot",
+    description="Get a bot informations as name",
+)
+@permission_required("bots:read")
+async def get_bot(slug: str, current_user: Annotated[User, Depends(get_current_user)]):
+    bot = await Bot.prisma().find_unique(where={"slug": slug})
+    if bot is None:
+        raise HTTPException(status_code=404, detail="Bot not found")
+    return bot
