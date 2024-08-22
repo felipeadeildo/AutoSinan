0c8be88cdbe1d9aca475bfb1b2a0da55ff837d24
Felipe Adeildo <felipeadeildo@duck.com>
2024-08-22 17:59:36 -0300

    refactor(config): nitro proxy to the backend

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,17 +4,14 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui'],
   compatibilityDate: '2024-08-19',
   routeRules: {
-    '/backend/**': {
+    '/api/**': {
       proxy:
         process.env.NODE_ENV === 'development'
           ? 'http://127.0.0.1:8000/**'
           : '/api/**',
     },
-    '/docs': {
-      proxy: 'http://127.0.0.1:8000/docs',
-    },
-    '/openapi.json': {
-      proxy: 'http://127.0.0.1:8000/openapi.json',
-    },
+  },
+  icon: {
+    localApiEndpoint: '/icons-api/**',
   },
 })
