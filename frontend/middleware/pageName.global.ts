e7e57379d61985d7c5419b8a79e7bc219768f592
Felipe Adeildo <felipeadeildo@duck.com>
2024-08-22 21:12:23 -0300

    chore(front): remove console log (bléee)

--- frontend/middleware/pageName.global.ts
+++ frontend/middleware/pageName.global.ts
@@ -2,7 +2,6 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const name = to.name?.toString() || 'index'
 
   const namedPages = Object.keys(PAGE_NAMES)
-  console.log(to)
 
   if (!name || !namedPages.includes(name)) return
 
