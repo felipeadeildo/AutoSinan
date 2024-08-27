75e4ff9f86e6d7810673c764bfa94ed122072f48
Felipe Adeildo <felipeadeildo@duck.com>
2024-08-27 15:00:13 -0300

    refactor(investigator): uses the new componentes to construct the form

--- frontend/pages/bots/investigator/settings.vue
+++ frontend/pages/bots/investigator/settings.vue
@@ -4,12 +4,31 @@
     allowedRoles: ['user', 'admin'],
     layout: 'bot',
   })
+
+  const { botConfigurations } = useBotConfigurations('investigator')
+  const { updateConfiguration } = useBotConfigurationUpdate('investigator')
 </script>
 
 <template>
   <div class="h-screen flex justify-center items-center">
     <h1 class="text-4xl font-extrabold mb-6 text-center">
       Configurações do bot de investigação
+
+      <div class="grid grid-cols-2 gap-3">
+        <template v-for="config in botConfigurations" :key="config.key">
+          <ConfigInput
+            v-if="config.type === 'STRING' && !config.options"
+            :config="config"
+            @save="updateConfiguration"
+          />
+
+          <ConfigSelect
+            v-else-if="config.options"
+            :config="config"
+            @save="updateConfiguration"
+          />
+        </template>
+      </div>
     </h1>
   </div>
 </template>
