f40be0367900c38e63fdabcde4e056bc2bfb036b
Felipe Adeildo <felipeadeildo@duck.com>
2024-09-17 21:04:34 -0300

    feat(settings): add upload files dest

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+from pydantic import field_validator
 from pydantic_settings import BaseSettings
 
 
@@ -27,6 +30,17 @@ class Settings(BaseSettings):
 
     DEFAULT_ADMIN_NAME: str = "Administrator"
     """Default admin name."""
+
+    UPLOADED_FILES_DEST: Path = Path.cwd() / "uploads"
+    """Folder where uploaded files will be stored."""
+
+    @field_validator("UPLOADED_FILES_DEST")
+    def validate_uploaded_files_dest(cls, value):
+        if isinstance(value, str):
+            value = Path(value)
+        if not value.exists():
+            value.mkdir()
+        return value
 
 
 settings = Settings()
